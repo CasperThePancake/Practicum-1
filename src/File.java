c4808994aa953996639cd3fbd8987670ff7845c1
CasperThePancake <60041702+CasperThePancake@users.noreply.github.com>
2026-02-19 13:33:16 +0100

    Some implementation

--- src/File.java
+++ src/File.java
@@ -7,29 +7,41 @@ public class File {
     private Date createTime;
     private Date modifyTime;
     private boolean writable;
-    private static int maxSize = 2048;
+    private static final int maxSize = 2048;
 
     public File(String name) {
-
+        this.rename(name);
+        this.createTime = new Date();
+        this.modifyTime = new Date(); // WIP If not modified yet, this should be null (usePeriod = 0)
+        this.writable = true;
+        this.size = 0;
     }
 
     public File(String name, int size, boolean writable) {
-
+        this.rename(name);
+        this.createTime = new Date();
+        this.modifyTime = new Date();
+        this.writable = writable;
+        this.setSize(size);
     }
 
     public void rename(String name) {
+        this.name = name; // NAME CHECKS HERE
+    }
 
+    private void setSize(int size) {
+        this.size = size; // WIP CHECKS
     }
 
     public void enlarge(int amount) {
-
+        this.setSize(this.size + amount); // WIP POS CHECKS
     }
 
     public void shorten(int amount) {
-
+        this.setSize(this.size - amount); // WIP POS CHECKS
     }
 
     public void hasOverlappingUsePeriod(File other) {
-
+        // WIP
     }
 }
